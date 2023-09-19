688f14f87533cb4be9fd665324a69d2305db81fd
nutlope <hassan4709@gmail.com>
2023-09-18 22:38:17 -0400

    added some prisma stuff

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,17 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
-  }
-  prisma = globalWithPrisma.prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export default prisma;
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+export default client;
