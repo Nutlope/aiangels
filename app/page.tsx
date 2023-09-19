1d4686506844665ac36b0d46cf2c8abf6ceb009d
nutlope <hassan4709@gmail.com>
2023-09-19 13:36:30 -0400

    cache every 24h

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Dashboard from './ClientPage';
 import prisma from '../utils/prisma';
+import { cache } from 'react';
+
+export const revalidate = 86400; // revalidate the data at most every 24 hours
+
+export const getAllAngels = cache(async () => {
+  const data = await prisma.investor.findMany({});
+  return data;
+});
 
 export default async function HomePage() {
-  const data = await prisma.investor.findMany({});
+  const data = getAllAngels();
 
   return (
     <div>
